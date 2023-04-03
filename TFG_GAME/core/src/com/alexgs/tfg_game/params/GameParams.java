d1ac602c47ea658faa252d14b710a7cee10fad7d
Alex Genzor <alexandros.genzor@fundacionmontessori.com>
2023-04-03 17:46:58 +0200

    Testing folder renamed. Official project created and started

---
+++ TFG_GAME/core/src/com/alexgs/tfg_game/params/GameParams.java
@@ -0,0 +1,15 @@
+package com.alexgs.tfg_game.params;
+
+public class GameParams {
+//    debug
+    public static final boolean DEBUG = true;
+
+//    screen resolution & aspect ratio values (multiply width with appropiate ratio
+    public static final int SCR_WIDTH = 1280;
+    public static final float[] SCR_RATIO = {0.5625f, 0.75f};
+
+//    audio volume
+    public static final float MUSIC_VOL = 0.6f;
+    public static final float SFX_VOL = 0.2f;
+
+}
---
+++ TFG_GAME/desktop/src/com/alexgs/tfg_game/DesktopLauncher.java
@@ -0,0 +1,23 @@
+package com.alexgs.tfg_game;
+
+import com.alexgs.tfg_game.params.GameParams;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+    public static void main(String[] arg) {
+        int chosenRatio = 1;
+
+        Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+        config.setForegroundFPS(60);
+        config.setTitle("Project Wasteland");
+        config.setWindowedMode(GameParams.SCR_WIDTH, (int) (GameParams.SCR_WIDTH * GameParams.SCR_RATIO[chosenRatio]));
+        System.out.println(GameParams.SCR_WIDTH + "x" + (int) (GameParams.SCR_WIDTH * GameParams.SCR_RATIO[chosenRatio]));
+
+        new Lwjgl3Application(new MyGdxGame(), config);
+
+    }
+
+}
