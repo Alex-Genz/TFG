58efd72b9cdbf0402673d0ce15c7cb7bb3745992
Alex Genzor <alexandros.genzor@fundacionmontessori.com>
2023-05-11 15:39:32 +0200

    deleted proof-of-concept TFG_GAME_TEMP

--- TFG_GAME_TEMP/GAME_2/assets/world/tilesets/windows-doors.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.2" name="windows-doors" tilewidth="32" tileheight="32" tilecount="5120" columns="32">
- <image source="../img/victorian-windows-doors.png" width="1024" height="5120"/>
-</tileset>
