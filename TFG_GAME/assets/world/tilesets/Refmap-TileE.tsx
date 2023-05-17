d5056e49831cd3edd8aa930508f03628c3047d6c
Alex Genzor <alexandros.genzor@fundacionmontessori.com>
2023-05-17 18:04:07 +0200

    minor commit

--- TFG_GAME/assets/world/tilesets/Refmap-TileE.tsx
+++ TFG_GAME/assets/world/tilesets/Refmap-TileE.tsx
@@ -1,6 +1,24 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.10.1" name="Refmap-TileE" tilewidth="32" tileheight="32" tilecount="256" columns="8">
  <image source="Refmap-TileE.png" width="256" height="1024"/>
+ <tile id="18">
+  <properties>
+   <property name="spawn" value="crystal"/>
+   <property name="variant" value="2"/>
+  </properties>
+ </tile>
+ <tile id="26">
+  <properties>
+   <property name="spawn" value="crystal"/>
+   <property name="variant" value="1"/>
+  </properties>
+ </tile>
+ <tile id="27">
+  <properties>
+   <property name="spawn" value="crystal"/>
+   <property name="variant" value="3"/>
+  </properties>
+ </tile>
  <tile id="105">
   <properties>
    <property name="spawn" value="crate"/>
